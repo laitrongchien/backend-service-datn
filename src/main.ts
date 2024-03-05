c5bfc09e35767fe5e475f59ef3ab37ac598d1760
laitrongchien <laichien2002@gmail.com>
2024-03-05 23:55:46 +0700

    Update cors config

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://vietnam-motorbike-tours.vercel.app',
+    ],
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('api'); // Set the global prefix to 'api'
